60c2e23b57038ff518b5c4e8569f2d803f9e8346
xxzhe <xiaxuzhe2019@gmail.com>
2021-11-27 22:22:44 -0800

    adding new classes and functions

--- main.py
+++ main.py
@@ -1,27 +1,104 @@
+import math
+import random
+
 import pyxel
 
-pyxel.init(256, 256)
 
-# game state:
-playerPos = [128, 128] # x, y
-pyxel.load("rock.pyxres")
-pyxel.playm(0, loop=True)
+SCREEN_WIDTH = 256
+SCREEN_HEIGHT = 256
 
-def update():
-    if pyxel.btnp(pyxel.KEY_S) and playerPos[1] <= 256:
-        playerPos[1] += 1
-    if pyxel.btnp(pyxel.KEY_W) and playerPos[1] >= 0:
-        playerPos[1] -= 1
-    if pyxel.btnp(pyxel.KEY_A) and playerPos[0] >= 0:
-        playerPos[0] -= 1
-    if pyxel.btnp(pyxel.KEY_D) and playerPos[0] <= 256:
-        playerPos[0] += 1
-    if pyxel.btnp(pyxel.KEY_Q):
-        pyxel.quit()
+METEOR_MAX_SPEED = 0.1
+METEOR_INITIAL_COUNT = 10
 
-def draw():
-    pyxel.cls(0)
-    pyxel.blt(playerPos[0],playerPos[1], 0, 0, 0, 16, 48)
-    pyxel.blt(16, 0, 0, 16, 0, 16, 16)
 
-pyxel.run(update, draw)
+class Vec2:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Meteor:
+    def __init__(self):
+        self.r = 5
+
+        self.pos = Vec2(
+            random.uniform(self.r, SCREEN_WIDTH - self.r),
+            random.uniform(self.r, SCREEN_HEIGHT - self.r),
+            )
+
+        self.vel = Vec2(
+            random.uniform(-METEOR_MAX_SPEED, METEOR_MAX_SPEED),
+            random.uniform(-METEOR_MAX_SPEED, METEOR_MAX_SPEED),
+            )
+
+    def update(self):
+        self.pos.x += self.vel.x
+        self.pos.y += self.vel.y
+
+class game:
+    def __init__(self):
+        pyxel.init(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+        pyxel.load("assets/rock.pyxres")
+        
+        self.reset()
+
+        self.player_x = 72
+        self.player_y = -16
+        self.player_vx = 0
+        self.player_vy = 0
+        self.player_is_alive = True
+
+        self.meteors1 = [Meteor() for _ in range(METEOR_INITIAL_COUNT)]
+
+        pyxel.run(self.update, self.draw)
+
+    def reset(self):
+        self.death = False
+
+    def update(self):
+        meteors1_count = len(self.meteors1)
+
+        self.update_player()
+
+        for i in range(meteors1_count - 1, -1, -1):
+            mi = self.meteors1[i]
+
+            # if abs(mi.pos.x - self.player_x) < 12 and abs(y - self.player_y) < 12:
+            # is_active = False
+            # self.score += (kind + 1) * 100
+            # self.player_vy = min(self.player_vy, -8)
+            # pyxel.play(3, 4)
+
+            
+            mi.update()
+
+    def update_player(self):
+        if pyxel.btn(pyxel.KEY_W):
+            self.player_y = max(self.player_y - 2, 0)
+
+        if pyxel.btn(pyxel.KEY_S):
+            self.player_y = min(self.player_y + 2, pyxel.height)
+
+        if pyxel.btn(pyxel.KEY_A):
+            self.player_x = max(self.player_x - 2, 0)
+
+        if pyxel.btn(pyxel.KEY_D):
+            self.player_x = min(self.player_x + 2, pyxel.width)
+    
+    def draw(self):
+        pyxel.cls(0)
+
+        pyxel.blt(
+            self.player_x,
+            self.player_y,
+            0,
+            0,
+            0,
+            15,
+            15,
+            12,
+        )
+        
+        for m in self.meteors1:
+            pyxel.blt(m.pos.x, m.pos.y, 0, 17, 1, 13, 13, 12)
+game()
