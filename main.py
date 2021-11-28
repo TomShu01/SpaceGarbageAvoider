db2a2ad1c8ccebeac0acf10429857fbb7898ded7
Tom <shutong5s@gmail.com>
2021-11-27 19:37:14 -0800

    testing adding picture

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ pyxel.init(256, 256)
 
 # game state:
 playerPos = [0, 0] # x, y
+pyxel.load("rock.pyxres")
 
 def update():
     if pyxel.btnp(pyxel.KEY_S) and playerPos[1] <= 256:
@@ -20,5 +21,6 @@ def update():
 def draw():
     pyxel.cls(0)
     pyxel.rect(playerPos[0],playerPos[1],20,20,11)
+    pyxel.blt(0, 0, 0, 0, 0, 16, 16)
 
 pyxel.run(update, draw)
